d5e05b7fba800d6d09104d273f3ad30e0908893c
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2020-06-11 16:35:22 -0400

    Skip expiring the primary cache key index on save without changes.

--- test/normalized_has_many_test.rb
+++ test/normalized_has_many_test.rb
@@ -199,7 +199,7 @@ class NormalizedHasManyTest < IdentityCache::TestCase
 
   def test_saving_a_child_record_should_expire_only_itself
     IdentityCache.cache.expects(:delete).with(@baz.primary_cache_index_key).once
-    @baz.save!
+    @baz.update!(updated_at: @baz.updated_at + 1)
   end
 
   def test_touching_child_with_touch_true_on_parent_expires_parent
